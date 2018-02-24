6837b14de62248d9ad14c7ed5ffe92b46c96bc87
jens87alpen <jenshoffmann20@gmail.com>
2018-02-24 08:50:58 -0800

    first commit

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+USE `burgers_db`;
+
+-- insert 3 burgers
+INSERT INTO `burgers` (burger_name, devoured, burger_time) VALUES ('Angus', false, CURRENT_TIMESTAMP);
+INSERT INTO `burgers` (burger_name, devoured, burger_time) VALUES ('Salmon', false, CURRENT_TIMESTAMP);
+INSERT INTO `burgers` (burger_name, devoured, burger_time) VALUES ('Mushroom', false, CURRENT_TIMESTAMP);
